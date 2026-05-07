885ff8b0882fa41efb75834b901edf16f14ab9e7
NoorhanDerar <nnoo0008@student.monash.edu>
2026-05-07 19:10:16 +0800

    Initial setup: Added menu and  sidebar container components to be reused

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,33 @@
+function Menu() {
+  return (
+    <nav className="menu">
+
+      <button className="menu-item">
+        Dashboard ⌂
+      </button>
+
+      <button className="menu-item">
+        Customers ⌘
+      </button>
+
+      <button className="menu-item">
+        Sales Pipeline ⊷
+      </button>
+
+      <button className="menu-item">
+        Tasks ☰
+      </button>
+
+      <button className="menu-item">
+        Calendar ◫
+      </button>
+
+      <button className="menu-item">
+        Settings ⚙
+      </button>
+
+    </nav>
+  );
+}
+
+export default Menu;
---
+++ frontend/src/components/SideBar.jsx
@@ -0,0 +1,19 @@
+import Menu from "./Menu";
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+
+      <div className="menu-wrapper">
+
+        <h2 className="menu-title">Menu</h2>
+
+        <Menu />
+
+      </div>
+
+    </aside>
+  );
+}
+
+export default Sidebar;
