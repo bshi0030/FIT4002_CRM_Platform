da13073596bdc6f6eec1d57bf0b145e7f64ff766
Hiba <hzam0001@student.monash.edu>
2026-05-10 00:51:42 +0800

    Adding sidebar component and sales pipeline to the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,41 @@
 import "./App.css";
 import logo from "./assets/CRM_logo.png";
 import Sidebar from "./components/Sidebar";
+import SalesPipeline from "./pages/SalesPipeline";
 
 function App() {
   return (
     <div className="app-layout">
+
+      {/* SIDEBAR */}
+      <aside className="sidebar">
+
         {/* LOGO */}
         <div className="sidebar-top">
           <img src={logo} alt="CRM Logo" className="logo" />
-
           <div className="logo-text">
             <p>Next Generation CRM Platform</p>
           </div>
         </div>
+
+        {/* MENU */}
+        <Sidebar />
+
+      </aside>
+
+      {/* MAIN CONTENT */}
+      <div className="main-content">
+
+        {/* TOPBAR — empty, logout removed */}
+        <div className="topbar">
+        </div>
+
+        {/* PAGE CONTENT */}
+        <div className="content-area">
+          <SalesPipeline />
+        </div>
+
+      </div>
 
     </div>
   );
