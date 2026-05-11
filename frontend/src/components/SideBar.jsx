bf9fa5a844ebf6bc5a3b887ae051f64a193df1a2
Mingze <mzhe0009@student.monash.edu>
2026-05-11 13:42:33 +0800

    Implemented frontend login page and reformat code

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,19 +1,19 @@
 import Menu from "./Menu";
 
 function Sidebar() {
-  return (
-    <aside className="sidebar">
+    return (
+        <aside className="sidebar">
 
-      <div className="menu-wrapper">
+            <div className="menu-wrapper">
 
-        <h2 className="menu-title">Menu</h2>
+                <h2 className="menu-title">Menu</h2>
 
-        <Menu />
+                <Menu/>
 
-      </div>
+            </div>
 
-    </aside>
-  );
+        </aside>
+    );
 }
 
 export default Sidebar;
