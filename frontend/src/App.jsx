e7bdfe1e58801b402dd23eabdc7f14bf192865d5
NoorhanDerar <nnoo0008@student.monash.edu>
2026-05-13 21:32:24 +0800

    Set up the main app layout with the sidebar and Kanban board then structured the main content area to redirect the task board.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
 import "./App.css";
 import logo from "./assets/CRM_logo.png";
 import Sidebar from "./components/Sidebar";
+import TaskKanban from "./pages/TaskKanban"; 
 
 function App() {
   return (
     <div className="app-layout">
+
+      {/* SIDEBAR */}
+      <aside className="sidebar">
         {/* LOGO */}
         <div className="sidebar-top">
           <img src={logo} alt="CRM Logo" className="logo" />
-
           <div className="logo-text">
             <p>Next Generation CRM Platform</p>
           </div>
         </div>
+
+        {/* MENU */}
+        <Sidebar />
+      </aside>
+
+      {/* MAIN CONTENT AREA */}
+      <div className="main-content">
+        <TaskKanban />
+      </div>
 
     </div>
   );
