8be541533ebd998523a15652ee43b91712f0b1a2
bshi0030 <bshi0030@student.monash.edu>
2026-05-13 17:19:57 +0800

    feat: implement customer detail view with file upload and download functionality

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middleware/uploadMiddleware');
+const { uploadLogo, uploadDocument } = require('../middleware/uploadMiddleware');
 const {
   createCustomer,
   getCustomers,
   getCustomerById,
-  updateCustomer
+  updateCustomer,
+  uploadCustomerFile,
+  viewCustomerFile,
+  downloadCustomerFile,
+  deleteCustomerFile
 } = require('../controllers/customerController');
 
 router.route('/')
-  .post(upload.single('companyLogo'), createCustomer)
+  .post(uploadLogo.single('companyLogo'), createCustomer)
   .get(getCustomers);
 
 router.route('/:id')
   .get(getCustomerById)
-  .put(upload.single('companyLogo'), updateCustomer);
+  .put(uploadLogo.single('companyLogo'), updateCustomer);
+
+// File attachment endpoints
+router.post('/:id/files', uploadDocument.single('file'), uploadCustomerFile);
+router.get('/:id/files/:fileId/view', viewCustomerFile);
+router.get('/:id/files/:fileId/download', downloadCustomerFile);
+router.delete('/:id/files/:fileId', deleteCustomerFile);
 
 module.exports = router;
