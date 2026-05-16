38d67147e3757e0429c99afa6d0a7de5e63391b7
Hiba <hzam0001@student.monash.edu>
2026-05-17 03:35:24 +0800

    Reconfigured docker yml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,3 @@
-import path from 'node:path'
-import {fileURLToPath} from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
