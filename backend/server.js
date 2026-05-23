4b921d956d8a2e303d46797fff862e979bcd5a5a
NoorhanDerar <nnoo0008@student.monash.edu>
2026-05-23 16:53:06 +0800

    Change server port number

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,7 @@ app.use(
 app.use(express.json({limit: '100kb'}))
 
 if (process.env.MONGO_URI) {
+
     mongoose
         .connect(process.env.MONGO_URI)
         .then(() => console.log('MongoDB connected successfully'))
@@ -51,7 +52,7 @@ app.use((err, req, res, _next) => {
     res.status(500).json({message: 'Internal server error'})
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 
@@ -60,3 +61,6 @@ app.use('/api/stages', stageRoutes)
 
 const dealRoutes = require('./routes/dealRoutes')
 app.use('/api/deals', dealRoutes)
+
+const taskRoutes = require('./routes/taskRoutes')
+app.use('/api/tasks', taskRoutes)
