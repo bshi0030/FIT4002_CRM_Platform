a553f51917dc3182f0928f80d91f27612bbd81b8
NoorhanDerar <nnoo0008@student.monash.edu>
2026-05-23 17:00:19 +0800

    add protected Task Kanban route with AppLayout wrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from '@/pages/Login'
 import Signup from '@/pages/Signup'
 import Dashboard from '@/pages/Dashboard'
 import SalesPipeline from '@/pages/SalesPipeline'
+import TaskKanban from './pages/TaskKanban'
 
 function App() {
     return (
@@ -34,6 +35,17 @@ function App() {
                             </ProtectedRoute>
                         }
                     />
+                        <Route
+        path="/task-kanban"
+        element={
+            <ProtectedRoute>
+                <AppLayout>
+                    <TaskKanban />
+                </AppLayout>
+            </ProtectedRoute>
+        }
+    />
+                    
                     <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
             </BrowserRouter>
